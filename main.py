32c47d53b6e0e4e6129d26b0c41f167038e302ac
Eric <zixia@umail.ucsb.edu>
2018-01-24 20:31:32 -0800

    add histogram

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pprint as pp
 from sklearn.datasets import fetch_20newsgroups
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def get_graphic_len(category):
     return len(fetch_20newsgroups(
@@ -26,4 +27,7 @@ for c in categories:
     res[c] = get_graphic_len([c])
 
 print res
-# pp.pprint(get_graphic(categories    ))
+plt.bar(res.keys(), res.values(), 0.5, color=['g','r','b','y','r','y','r','y'])
+plt.show()
+
+
