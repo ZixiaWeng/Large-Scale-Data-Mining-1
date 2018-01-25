a7ce55d1ebb8f3aa97e5748167cc7056abb4aa14
Shunji Zhan <shunhahaha@live.com>
2018-01-24 20:14:48 -0800

    can get all lenght

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+import pprint as pp
 from sklearn.datasets import fetch_20newsgroups
 
-categories = ['comp.graphics']
-graphics_train = fetch_20newsgroups(
-    subset='train',
-    categories=categories,
-    shuffle=True,
-    random_state=42
-)
-print graphics_train
+
+def get_graphic_len(category):
+    return len(fetch_20newsgroups(
+        subset='train',
+        categories=category,
+        shuffle=True,
+        random_state=42
+    )['data'])
+
+categories = [
+    'comp.graphics',
+    'comp.os.ms-windows.misc',
+    'comp.sys.ibm.pc.hardware',
+    'comp.sys.mac.hardware',
+    'rec.autos',
+    'rec.motorcycles',
+    'rec.sport.baseball',
+    'rec.sport.hockey'
+]
+
+res = {}
+for c in categories:
+    res[c] = get_graphic_len([c])
+
+print res
+# pp.pprint(get_graphic(categories    ))
