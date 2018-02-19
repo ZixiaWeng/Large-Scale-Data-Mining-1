12a33f093290533dcc7022885fc809912fe6ff28
Shunji Zhan <shunhahaha@live.com>
2018-02-19 11:40:40 -0800

    changed class structure

--- recommand/main.py
+++ recommand/main.py
@@ -3,4 +3,4 @@ from recommand import Recommand
 
 if __name__ == '__main__':
     r = Recommand()
-    r.q1()
+    r.preprocessing()
--- recommand/recommand.py
+++ recommand/recommand.py
@@ -8,70 +8,12 @@ class Recommand:
     def __init__(self):
         pass
 
-    def q1(self):
+    def preprocessing(self):  # q1-6
         print 'aaa'
 
-    def q2(self):
+    def knn(self):  # q7-11
         pass
 
-    def q3(self):
-        pass
 
-    def q4(self):
-        pass
-
-    def q5(self):
-        pass
-
-    def q6(self):
-        pass
-
-    def q7(self):
-        pass
-
-    def q8(self):
-        pass
-
-    def q9(self):
-        pass
-
-    def q10(self):
-        pass
-
-    def q11(self):
-        pass
-
-    def q12(self):
-        pass
-
-    def q13(self):
-        pass
-
-    def q14(self):
-        pass
-
-    def q15(self):
-        pass
-
-    def q16(self):
-        pass
-
-    def q17(self):
-        pass
-
-    def q18(self):
-        pass
-
-    def q19(self):
-        pass
-
-    def q20(self):
-        pass
-
-    def q21(self):
-        pass
-
-    def q22(self):
-        pass
 
 
