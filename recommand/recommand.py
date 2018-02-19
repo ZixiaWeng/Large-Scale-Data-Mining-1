3711bc990492090e639474a4f50ddc74498506fc
Eric <zixia@umail.ucsb.edu>
2018-02-19 12:12:15 -0800

    q1 modi

--- recommand/recommand.py
+++ recommand/recommand.py
@@ -5,33 +5,42 @@ import matplotlib.pyplot as plt
 
 def read_data():
     # Loading Ratings.csv
+    available_rats = 100004
     ratings = {}
+    sparse = {}
     ratings['user'] = []
     ratings['movie'] = []
     ratings['rating'] = []
-
+    sparse['user'] = []
+    sparse['movie'] = []
+    sparse['rating'] = []
     filename = 'recommand/ml-latest-small/ratings.csv'
     with open (filename , "rt") as input:
-      reader = csv.reader(input, delimiter=',', quoting=csv.QUOTE_NONE)
-      next(reader, None) # skip header
-      for line in reader:
-        #print line
-        ratings['user'].append( float(line[0]))
-        ratings['movie'].append( float(line[1]))
-        ratings['rating'].append( float(line[2]))
-
-    return ratings
+      	reader = csv.reader(input, delimiter=',', quoting=csv.QUOTE_NONE)
+      	next(reader, None) # skip header
+      	for line in reader:
+      		ratings['user'].append( float(line[0]))
+      		ratings['movie'].append( float(line[1]))
+        	ratings['rating'].append( float(line[2]))
+        	if float(line[0]) not in sparse['user']:
+        		sparse['user'].append( float(line[0]))
+        	if float(line[1]) not in sparse['movie']:
+        		sparse['movie'].append( float(line[1]))
+        	if float(line[2]) not in sparse['rating']:
+	        	sparse['rating'].append( float(line[2]))
+	sparisty = float(available_rats)/float((len(sparse['user'])) * len(sparse['movie']))
+    return ratings, sparisty
 
 
 class Recommand:
     def __init__(self):
-        self.ratings = read_data()
+        self.ratings, self.sparisty = read_data()
 
     def preprocessing(self):  # q1-6
         # 100004 is the number of available ratings
         # 671 is the number of users
         # 9125 is the number of movies
-        print "Sparisty = " + str(100004 / 671*9125)
+        print "Sparisty = " + str(self.sparisty)
 
         # Q2
         plot2_y = numpy.zeros(11)
