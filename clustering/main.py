f53a85b52b1fffd48732919959a3bf4eba14dae2
Shunji Zhan <shunhahaha@live.com>
2018-02-19 11:16:28 -0800

    ready for pj3

---
+++ clustering/main.py
@@ -0,0 +1,8 @@
+from Clustering import Clustering
+
+if __name__ == '__main__':
+    cl = Clustering()
+    cl.q1()
+    cl.q2()
+    cl.q3()
+    cl.q4()
--- main.py
+++
@@ -1,8 +0,0 @@
-from Clustering import Clustering
-
-if __name__ == '__main__':
-    cl = Clustering()
-    cl.q1()
-    cl.q2()
-    cl.q3()
-    cl.q4()
