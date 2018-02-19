f8e91deaf5faa1262ea57d720d05a2259792fd19
Don <donlee940125@gmail.com>
2018-02-19 12:39:28 -0800

    finished Q6, commented out Q3 and Q4 (use R)

--- recommand/recommand.py
+++ recommand/recommand.py
@@ -39,19 +39,58 @@ class Recommand:
           plot2_y [  int (self.ratings['rating'][i] / 0.5) ] += 1
         plt.bar( range(0,11), plot2_y)
         plt.show()
-
+        
+        '''
         # Q3
         movie_count = {}
         for i in range (len(self.ratings['rating'])):
           key = int (self.ratings['movie'][i])
           if not key in movie_count.keys():
-            movie_count[ self.ratings['movie'][i] ] = 0
+            movie_count[ self.ratings['movie'][i] ] = 1
           else:
             movie_count[ self.ratings['movie'][i] ] += 1
-
-        print movie_count.values()
-
-
+        plt.bar( range(len(movie_count)), movie_count.values())
+        plt.xticks(range(len(movie_count)), movie_count.keys())
+        plt.show()
+        
+        # Q4
+        user_count = {}
+        for i in range (len(self.ratings['rating'])):
+          key = int (self.ratings['user'][i])
+          if not key in movie_count.keys():
+            user_count[ self.ratings['user'][i] ] = 1
+          else:
+            user_count[ self.ratings['user'][i] ] += 1
+        plt.bar( range(len(user_count)), user_count.values())
+        plt.xticks(range(len(user_count)), user_count.keys())
+        plt.show()
+        '''
+        
+        '''
+        # Q6
+        rating_by_moive = {}
+        for i in range (len(self.ratings['rating'])):
+            movie_key = (self.ratings['movie'][i])
+            if not movie_key in rating_by_moive.keys():
+                rating_by_moive[ movie_key ] = []
+                rating_by_moive[ movie_key ].append( self.ratings['rating'][i] )
+            else:
+                rating_by_moive[ movie_key ].append( self.ratings['rating'][i] )
+        variances = []
+        for ratings in rating_by_moive.values():
+            variances.append ( numpy.var(ratings))
+        count_by_binned_variance = {}
+        for variance in variances:
+            bin_key = int (variance / 0.5)
+            if not bin_key in count_by_binned_variance.keys():
+                count_by_binned_variance[ bin_key] = 1
+            else:
+                count_by_binned_variance[ bin_key] += 1
+        plt.bar( range(len(count_by_binned_variance)), count_by_binned_variance.values())
+        plt.xticks( range(len(count_by_binned_variance)), count_by_binned_variance.keys())
+        plt.show()
+        '''
+          
     def knn(self):  # q7-11
         pass
 
