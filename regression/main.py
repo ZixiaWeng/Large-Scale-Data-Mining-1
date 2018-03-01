e32a2860625730ff3f59aee76359635170cd498b
Eric <zixia@umail.ucsb.edu>
2018-02-28 19:30:33 -0800

    main.py

---
+++ regression/main.py
@@ -0,0 +1,5 @@
+from regression import Regression
+
+
+if __name__ == '__main__':
+    r = Regression()
--- regression/regression.py
+++ regression/regression.py
@@ -1,5 +1,12 @@
 import csv
 import numpy as np
+from surprise import Dataset
+from surprise import Reader
+import pandas as pd
 
 class Regression:
     def __init__(self):
+        self.data = pd.read_csv('network_backup_dataset.csv')
+        print self.data['Week #']
+
+
