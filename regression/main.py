081ea310cb3912e403c504efd319d4ba6044422f
Shunji Zhan <shunhahaha@live.com>
2018-02-28 19:52:16 -0800

    added random forest function

--- regression/main.py
+++ regression/main.py
@@ -3,3 +3,4 @@ from regression import Regression
 
 if __name__ == '__main__':
     r = Regression()
+    r.random_forest()
--- regression/regression.py
+++ regression/regression.py
@@ -4,9 +4,23 @@ from surprise import Dataset
 from surprise import Reader
 import pandas as pd
 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
+
+
 class Regression:
     def __init__(self):
         self.data = pd.read_csv('network_backup_dataset.csv')
-        print self.data['Week #']
+        # print self.data['Week #']
 
-
+    def random_forest(self):
+        X, y = make_regression(n_features=5, n_informative=2, random_state=0, shuffle=False)
+        regr = RandomForestRegressor(
+            n_estimators=20,
+            max_depth=4,
+            bootstrap=True,
+            max_features=5,
+        )
+        regr.fit(X, y)
+        print regr.feature_importances_
+        print regr.predict([[0, 0, 0, 0, 0]])
