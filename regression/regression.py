e55dd95f5ce214f6ca8a72d014bd600aedef2c0e
Don <donlee940125@gmail.com>
2018-02-28 19:51:21 -0800

    modified __init__ to seperate X and Y

--- regression/regression.py
+++ regression/regression.py
@@ -1,12 +1,23 @@
 import csv
 import numpy as np
-from surprise import Dataset
-from surprise import Reader
 import pandas as pd
 
 class Regression:
     def __init__(self):
         self.data = pd.read_csv('network_backup_dataset.csv')
-        print self.data['Week #']
+        print self.data["Size of Backup (GB)"][:10]
+
+        self.Y = data["Size of Backup (GB)"]
+        self.X = del data.drop("Size of Backup (GB)", 1)
+
+    def Scaler_Encoding(self):
+
+    	return data
 
 
+   	def One_Hot_Encoding(self):
+
+   		return 0
+
+    def Linear_Regression():
+    	return 0
