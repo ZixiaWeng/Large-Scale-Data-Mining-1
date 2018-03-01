99cc1ae028a5ba21f6d2c6807e5eefd1dd549b77
Shunji Zhan <shunhahaha@live.com>
2018-03-01 13:52:15 -0800

    merge

--- regression/regression.py
+++ regression/regression.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import make_regression
+from sklearn.model_selection import KFold
 
 
 def day_of_week_encoding(day):
@@ -26,27 +27,37 @@ class Regression:
         # print self.data
         # print self.X, self.Y
 
-    def scaler_encoding(self):
-        newX = self.X.copy()
-        newY = self.Y.copy()
+    def scaler_encoding(self, X):
+        newX = X.copy()
 
         newX["Day of Week"] = newX["Day of Week"].apply(day_of_week_encoding)
         newX["Work-Flow-ID"] = newX["Work-Flow-ID"].apply(lambda id: id.split('_')[2])
         newX["File Name"] = newX["File Name"].apply(lambda name: name.split('_')[1])
 
-        return newX, newY
+        return newX
 
     def random_forest(self):
         # X, y = make_regression(n_features=5, n_informative=2, random_state=0, shuffle=False)
-        X, Y = self.scaler_encoding()
-        print X, Y
-        regr = RandomForestRegressor(
+        tree = RandomForestRegressor(
             n_estimators=20,
             max_depth=4,
             bootstrap=True,
             max_features=5,
         )
-        regr.fit(X, Y)
+        kf = KFold(n_splits=2)
+        for train, test in kf.split(self.data):
+            print train.shape, test.shape
+            # train = self.scaler_encoding(train)
+            train_Y = train["Size of Backup (GB)"]
+            train_X = train.drop("Size of Backup (GB)", 1)
+
+            # test = self.scaler_encoding(test)
+            test_Y = test["Size of Backup (GB)"]
+            test_X = test.drop("Size of Backup (GB)", 1)
+
+            tree.fit(train_X, train_Y)
+            print tree.predict(test_X)
+
         # print regr.feature_importances_
-        # print regr.predict([[0, 0, 0, 0, 0]])
+        # print tree.predict(X)
 
