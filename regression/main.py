ea4347f65bce5a61a11d6f569ad7a62327ae6f22
Eric <zixia@umail.ucsb.edu>
2018-03-05 11:46:38 -0800

    das

--- regression/main.py
+++ regression/main.py
@@ -8,18 +8,22 @@ if __name__ == '__main__':
     r.plot_buSize(20)
     # Q1b
     r.plot_buSize(105)
+    # Q2a i&ii now is standardized, should comment 2 lines of standardization of code to see normal regression model
     r.linear_regression_whole_Data_Points()
     r.linear_regression()
+    # Q2a iii
     # r.f_reg()
     # r.random_forest()
 
     # Q2a iv.
-    # r.featureCombinationEncoding()
-    # r.rsmeUnderCombineEncoding()
-    # r.linear_regression_whole_Data_Points_best_combination()
-    # r.ridgeRegularizer()
-    # r.runRidgeAndLasso()
+    r.featureCombinationEncoding()
+    r.rsmeUnderCombineEncoding()
+    r.linear_regression_whole_Data_Points_best_combination()
+    # Q2a v
+    r.ridgeRegularizer()
+    r.runRidgeAndLasso()
     r.getCoefficient()
+
     # r.f_reg()
     # r.nn()
     # r.knn()
