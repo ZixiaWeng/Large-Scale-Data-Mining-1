977040c4379f5a5616e104747271630c5fc5a180
Shunji Zhan <shunhahaha@live.com>
2018-03-08 12:09:11 -0800

    q1 plot ok

--- popularity_prediction/prediction.py
+++ popularity_prediction/prediction.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import matplotlib.pyplot as plt
+
 import numpy as np
 import json
 import pprint as pp
-import datetime, time
+import datetime
 import pytz
-
-
-def read_tweet(hashtag, max_line=1000):
-    if hashtag not in {'gohawks', 'gopatriots', 'nfl', 'patriots', 'sb49', 'superbowl'}:
-        raise Exception('no such data!')
-
-    file = './tweet_data/tweets_#%s.txt' % hashtag
-    tweets = []
-    counter = 0
-    for line in open(file, 'r'):
-        data = json.loads(line)
-        tweets.append(data)
-        counter += 1
-        if counter > max_line:
-            break
-    return tweets
 
 
 def to_date(timestamp):
@@ -33,17 +19,53 @@ def get_hours(data):
     return data.total_seconds() / 3600.0
 
 
+def get_hour_diff(all_tweets):
+    initDate = to_date(all_tweets[0]['firstpost_date'])
+    endDate = to_date(all_tweets[-1]['firstpost_date'])
+    return get_hours(endDate - initDate)
+
+
 class Prediction:
     def __init__(self):
-        pass
+        self.all_data = {}
+
+    def read_tweet(self, hashtag, max_line=1000):
+        if hashtag not in {'gohawks', 'gopatriots', 'nfl', 'patriots', 'sb49', 'superbowl'}:
+            raise Exception('no such data!')
+
+        if hashtag in self.all_data.keys():
+            return self.all_data[hashtag]
+
+        file = './tweet_data/tweets_#%s.txt' % hashtag
+        tweets = []
+        counter = 0
+        for line in open(file, 'r'):
+            data = json.loads(line)
+            tweets.append(data)
+            counter += 1
+            if (max_line > 0 and counter > max_line):
+                break
+
+        self.all_data[hashtag] = tweets
+        return tweets
+
+    def plot_histogram(self, hashtag):
+        all_tweets = self.read_tweet(hashtag)
+        initDate = to_date(all_tweets[0]['firstpost_date'])
+        hour_diff = get_hour_diff(all_tweets)
+        X = range(0, int(hour_diff) + 1)
+        Y = [0] * (int(hour_diff) + 1)
+        for tweet in all_tweets:
+            index = int(get_hours(to_date(tweet['firstpost_date']) - initDate))
+            Y[index] += 1
+        plt.bar(X, Y, width=1)
+        plt.title('%s number of tweets per hour' % hashtag)
+        plt.show()
 
     def q1(self):
-        all_tweets = read_tweet('gohawks')
+        all_tweets = self.read_tweet('gohawks')
         tweetsLen = len(all_tweets)
-
-        initDate = to_date(all_tweets[0]['firstpost_date'])
-        endDate = to_date(all_tweets[-1]['firstpost_date'])
-        hour_diff = float(get_hours(endDate - initDate))
+        hour_diff = get_hour_diff(all_tweets)
         print 'average tweets per hour: %.3f' % (tweetsLen / hour_diff)
 
         all_follower = 0.0
@@ -54,4 +76,7 @@ class Prediction:
 
         print 'average followers of users: %.3f' % (all_follower / tweetsLen)
         print 'average number of retweets: %.3f' % (all_retweets / tweetsLen)
+
+        self.plot_histogram('superbowl')
+        self.plot_histogram('nfl')
 
