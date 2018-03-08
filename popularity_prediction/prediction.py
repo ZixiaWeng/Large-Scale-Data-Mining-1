7917d24d0b57418b089900ebc1d7aaed23962e21
Shunji Zhan <shunhahaha@live.com>
2018-03-08 10:09:01 -0800

    changed identation

--- popularity_prediction/prediction.py
+++ popularity_prediction/prediction.py
@@ -4,8 +4,7 @@ import json
 
 class Prediction:
     def __init__(self):
-    	# self.train_data_gohawks = np.loadtxt('./tweet_data/tweets_#gopatriots.txt', dtype=str)
-		with open('./tweet_data/tweets_#gopatriots.txt') as json_file:  
-		    data = json.load(json_file)
-		    for p in data:
-		        print p
+        # self.train_data_gohawks = np.loadtxt('./tweet_data/tweets_#gopatriots.txt', dtype=str)
+        with open('./tweet_data/tweets_#nfl.txt') as json_file: 
+            data = json.load(json_file)
+            print data
