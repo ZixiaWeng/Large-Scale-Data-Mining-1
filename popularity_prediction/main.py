787ee1e17aae89a22f392a6840df80d92822b018
Don <donlee940125@gmail.com>
2018-03-08 12:59:39 -0800

    able to extract 5 features and create new df

--- popularity_prediction/main.py
+++ popularity_prediction/main.py
@@ -2,5 +2,5 @@ from prediction import Prediction
 
 if __name__ == '__main__':
     p = Prediction()
-    p.q1()
-    # p.linear_regression()
+    # p.q1()
+    p.linear_regression()
--- popularity_prediction/prediction.py
+++ popularity_prediction/prediction.py
@@ -90,10 +90,29 @@ class Prediction:
     def linear_regression(self):
         df_superbowl = pd.DataFrame(self.train_data_superbowl)
         df_new = pd.DataFrame(columns=['tweets_num','retweets_num','followers_num','followers_num_max','time_of_day'])
-        # intial_time = df_superbowl[][0]
-        for dta in df_superbowl:
-            new_entry = [1, 2, 3, 4, 5]
-            df_new.loc[len(df_new)] = new_entry
-            pass
-        print df_new, 'dsad'
+        initDate = to_date(self.train_data_superbowl[0]['firstpost_date']).replace(minute=0, second=0)
+        hour_diff = get_hour_diff(self.train_data_superbowl)
+        total_num_of_tweets = [0] * (int(hour_diff) + 1)
+        total_num_of_retweets = [0] * (int(hour_diff) + 1)
+        total_num_of_follower = [0] * (int(hour_diff) + 1)
+        max_num_follower = [0] * (int(hour_diff) + 1)
+        time_of_day = [0] * (int(hour_diff) + 1)
+        for dta in self.train_data_superbowl:
+            # new_entry = [1, 2, 3, 4, 5]
+            # df_new.loc[len(df_new)] = new_entry
+            # pass
+            index = int(get_hours(to_date(dta['firstpost_date']) - initDate))
+            total_num_of_tweets[index] += 1
+            total_num_of_retweets[index] += dta['metrics']['citations']['total']
+            total_num_of_follower[index] += dta['author']['followers']
+            max_num_follower[index] = max(max_num_follower[index], dta['author']['followers'])
+            # time_of_day[index] = to_date(dta['firstpost_date']).hour
+        time_of_day[0] = initDate.hour
+        for i in range(1, len(time_of_day)):
+            time_of_day[i] = (time_of_day[i-1] + 1) % 24
+        # print total_num_of_tweets
+        # print total_num_of_follower
+        # print time_of_day
+        # print to_date(self.train_data_superbowl[0]['firstpost_date'])
+        # print df_new, 'dsad'
         self.map_hour(df_superbowl)
