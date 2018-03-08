226f9f036eae4a3906c841d066975ad7ea49c27a
Shunji Zhan <shunhahaha@live.com>
2018-03-08 10:25:31 -0800

    factored out read tweet function

--- popularity_prediction/prediction.py
+++ popularity_prediction/prediction.py
@@ -6,11 +6,23 @@ import json
 import pprint as pp
 
 
+def read_tweet(hashtag, max_line=100):
+    if hashtag not in {'gohawks', 'gopatriots', 'nfl', 'patriots', 'sb49', 'superbowl'}:
+        raise Exception('no such data!')
+
+    file = './tweet_data/tweets_#%s.txt' % hashtag
+    tweets = []
+    counter = 0
+    for line in open(file, 'r'):
+        data = json.loads(line)
+        tweets.append(data)
+        counter += 1
+        if counter > max_line:
+            break
+    return tweets
+
+
 class Prediction:
     def __init__(self):
-        file = './tweet_data/tweets_#nfl.txt'
-        tweets = []
-        for line in open(file, 'r'):
-            data = json.loads(line)
-            tweets.append(data)
-            pp.pprint(data)
+        self.data = read_tweet('gohawks')
+        pp.pprint(self.data)
